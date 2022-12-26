5542b624128d1ee28462f49d8d355160f81ea635
Markos <Markos@DESKTOP-V3N2I49>
2022-12-26 14:29:46 +0100

    Prueba1 desde casa

---
+++ src/clases/LineaFactura.java
@@ -0,0 +1,22 @@
+package clases;
+
+public class LineaFactura {
+	
+	private int numero;
+	private String Articulo;
+	private int cantidad;
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+}
