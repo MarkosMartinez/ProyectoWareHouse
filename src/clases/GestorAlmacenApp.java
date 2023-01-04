8090bdb02978c886087c16881519f8eb96d58826
Markos <Markos@DESKTOP-V3N2I49>
2023-01-04 17:40:09 +0100

    GestorAlmacen ya tiene el Run y Refresco.java creado (solo el esqueleto)

---
+++ src/clases/GestorAlmacenApp.java
@@ -0,0 +1,9 @@
+package clases;
+
+public class GestorAlmacenApp {
+	public void run() {
+	
+		
+		
+	}
+}
---
+++ src/clases/Refresco.java
@@ -0,0 +1,85 @@
+package clases;
+
+public class Refresco extends Articulo {
+	
+	private String sabor;
+	private boolean zumo;
+	private boolean gaseoso;
+	private int cantidadAzucar;
+	
+	public Refresco() {
+//		super();
+	}
+	
+	public String getSabor() {
+		return sabor;
+	}
+
+
+
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+
+
+
+	public boolean isZumo() {
+		return zumo;
+	}
+
+
+
+	public void setZumo(boolean zumo) {
+		this.zumo = zumo;
+	}
+
+
+
+	public boolean isGaseoso() {
+		return gaseoso;
+	}
+
+
+
+	public void setGaseoso(boolean gaseoso) {
+		this.gaseoso = gaseoso;
+	}
+
+
+
+	public int getCantidadAzucar() {
+		return cantidadAzucar;
+	}
+
+
+
+	public void setCantidadAzucar(int cantidadAzucar) {
+		this.cantidadAzucar = cantidadAzucar;
+	}
+
+
+
+	@Override
+	public void visualizarArticulo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void precioTotal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean esSaludable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public void visualizarPropiedades() {
+		//sysout
+	}
+	
+
+}
