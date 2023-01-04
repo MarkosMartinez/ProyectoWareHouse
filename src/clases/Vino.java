eddd3627c1e4689ff96eb6b1bc98ba09c837baa6
Markos <Markos@DESKTOP-V3N2I49>
2023-01-04 17:46:01 +0100

    Vino.java "esqueleto" creado.

---
+++ src/clases/Vino.java
@@ -0,0 +1,70 @@
+package clases;
+
+public class Vino extends Articulo implements Alcoholico {
+
+	private String color;
+	private String origen;
+	private int anio;
+	private String tipoDeUva;
+	private double gradosAlcohol;
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	public String getTipoDeUva() {
+		return tipoDeUva;
+	}
+
+	public void setTipoDeUva(String tipoDeUva) {
+		this.tipoDeUva = tipoDeUva;
+	}
+
+	public double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+
+	public void setGradosAlcohol(double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
+
+	@Override
+	public void visualizarArticulo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void precioTotal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean esSaludable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+}
