5122684bac8864a3bc6f21e9eac796b9c54141ab
Markos <Markos@DESKTOP-V3N2I49>
2023-01-04 17:48:30 +0100

    Metodos de Alcoholico creado

--- src/clases/Alcoholico.java
+++ src/clases/Alcoholico.java
@@ -3,4 +3,7 @@ package clases;
 public interface Alcoholico {
 	static final double TASA_BEBIDAS_FUERTES=64.13;
 	static final double TASA_BEBIDAS_SUABES=38.48;
+	
+	public boolean esFuerte();
+	public double calcularTasa();
 }
