03c406171b2c2f304e0961f7df3fdd6189fad60e
Markos <Markos@DESKTOP-V3N2I49>
2023-01-07 17:50:25 +0100

    Esqueleto de Almacen.java creado

---
+++ src/clases/Almacen.java
@@ -0,0 +1,55 @@
+package clases;
+
+import java.util.ArrayList;
+
+public class Almacen {
+	
+	public void cargarDatos() {
+		
+	}
+	
+	public Articulo elMasCaro() {
+		
+		return null;
+	}
+	
+	public double precio(String codigoProducto) {
+		
+		return 0;
+	}
+	
+	public boolean hayStock(String codigoProducto) {
+		
+		return false;
+	}
+	
+	public ArrayList<Articulo> stockJusto(){
+		
+		return null;
+	}
+	
+	public Articulo articulo(String codigoProducto) {
+		
+		return null;
+	}
+	
+	public boolean disponibilidad(int cantidad, String codigoProducto) {
+		
+		return false;
+	}
+	
+	public ArrayList<Articulo> equivalentes(Articulo articulo){
+		
+		return null;
+	}
+	
+	public ArrayList<Articulo> ordenarPorPrecio(String orden) {
+		
+		return null;
+	}
+	
+	public void ordenarPorStock(String orden) {
+		
+	}
+
+}
