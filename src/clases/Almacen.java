0a1942b073df162ffbe2fac803e4e390bc0a3488
Markos <Markos@DESKTOP-V3N2I49>
2023-01-07 18:51:24 +0100

    Refresco implementado a la hora de leerlo del fichero y crear el objeto

--- src/clases/Almacen.java
+++ src/clases/Almacen.java
@@ -1,11 +1,39 @@
 package clases;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Almacen {
 	
-	public void cargarDatos() {
+	public Almacen() {}
+	
+	ArrayList<Articulo> articulos = new ArrayList<>();
+	
+	public ArrayList<Articulo> getArticulos(){
+		return articulos;
+	}
+	
+	public void cargarDatos() throws FileNotFoundException {
+		File file = new File("datos/datos.txt");
+		Scanner scan = new Scanner(file);
 		
+		while(scan.hasNextLine()) {
+			String linea[]= scan.nextLine().split(";");
+			if(Integer.parseInt(linea[0]) <= 9) {
+				
+				Refresco refresco = new Refresco(linea[1], linea[2], linea[3], linea[4],
+						Boolean.parseBoolean(linea[5]), Boolean.parseBoolean(linea[6]),
+						Integer.parseInt(linea[7]), Integer.parseInt(linea[8]),
+						Double.parseDouble(linea[9]), Integer.parseInt(linea[10]));
+				
+				articulos.add(refresco);
+			}
+		
+			
+			
+		}
 	}
 	
 	public Articulo elMasCaro() {
