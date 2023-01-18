20b0c4333df4a0635556bc36c85b937171a8d61c
Markos Martinez <Markos Martinez@HZ114826.H012108.NET>
2023-01-18 10:59:15 +0100

    Fallo del Main solucionado.

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -1,10 +1,11 @@
 package clases;
 
 import java.io.FileNotFoundException;
+import java.text.ParseException;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, ParseException {
 		GestorAlmacenApp gaapp = new GestorAlmacenApp();
 		gaapp.run();
 
